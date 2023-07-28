87c75e92a53270b2b5cf7621912708b634a37cc0
volianskii <dima1998vol@gmail.com>
2023-07-28 17:41:15 +0300

    добавляет константы с url адресами

--- src/hooks/use-map.tsx
+++ src/hooks/use-map.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState, MutableRefObject, useRef} from 'react';
 import {Map, TileLayer} from 'leaflet';
 import {City} from '../mocks/city';
+import {URL_MAP_LAYER, URL_MAP_ATTRIBUTION} from '../constants/map-url.ts';
 
 function useMap(mapRef: MutableRefObject<HTMLElement | null>, city: City): Map | null {
   const [map, setMap] = useState<Map | null>(null);
@@ -17,10 +18,10 @@ function useMap(mapRef: MutableRefObject<HTMLElement | null>, city: City): Map |
       });
 
       const layer = new TileLayer(
-        'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+        URL_MAP_LAYER,
         {
           attribution:
-            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          URL_MAP_ATTRIBUTION
         }
       );
 
