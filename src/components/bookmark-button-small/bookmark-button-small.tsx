6daa40cba5d3fd67ab6f75f940ca519e3af7da63
volianskii <dima1998vol@gmail.com>
2023-08-17 16:42:39 +0300

    добавлет компонент bookmark-button-small

---
+++ src/components/bookmark-button-small/bookmark-button-small.tsx
@@ -0,0 +1,43 @@
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { getAuthStatus } from '../../store/user-data/selectors';
+import classNames from 'classnames';
+
+type BookmarkButtonProps = {
+  id: string;
+  isActive: boolean;
+};
+
+function BookmarkButton({id, isActive}: BookmarkButtonProps) {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const isAuthorized = useAppSelector(getAuthStatus);
+
+  const handleClick = () => {
+    if (isAuthorized) {
+      navigate('/login');
+      return;
+    }
+    if(isActive) {
+      dispatch(deleteFavorite(id));
+      //добавить в редюсер
+    } else {
+      //добавить в редюсер
+      dispatch(addFavorite(id));
+    }
+  };
+
+  return (
+    <button className={classNames('place-card__bookmark-button', 'button', {
+      'place-card__bookmark-button--active': isActive && isAuthorized
+    })} type="button" onClick={handleClick}
+    >
+      <svg className="place-card__bookmark-icon" width="18" height="19">
+        <use xlinkHref="#icon-bookmark"></use>
+      </svg>
+      <span className="visually-hidden">{isActive && isAuthorized ? 'In' : 'To'} bookmarks</span>
+    </button>
+  );
+}
+
+export default BookmarkButton;
