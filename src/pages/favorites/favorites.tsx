71ff518ace8575d8aaae337120dcb3c184b74132
volianskii <dima1998vol@gmail.com>
2023-08-17 15:01:58 +0300

    добавляет компонент favorites-empty

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,12 +1,11 @@
 import { useEffect } from 'react';
-import Logo from '../../components/logo/logo.tsx';
 import { useAppSelector } from '../../hooks/index.ts';
 import { fetchFavoritesAction } from '../../store/api-actions.ts';
 import { store } from '../../store/index.ts';
-import { Link } from 'react-router-dom';
 import { OfferType } from '../../types/offer.ts';
 import Header from '../../components/header/header.tsx';
 import { getFavorites } from '../../store/favorites-data/selectors.ts';
+import FavoritesEmpty from '../../components/favorites-empty/favorites-empty.tsx';
 
 function Favorites(): JSX.Element {
   const favorites: OfferType[] = useAppSelector(getFavorites);
@@ -32,49 +31,7 @@ function Favorites(): JSX.Element {
           </a>
         </footer>
       </div> :
-      <div className="page page--favorites-empty">
-        <header className="header">
-          <div className="container">
-            <div className="header__wrapper">
-              <Logo />
-              <nav className="header__nav">
-                <ul className="header__nav-list">
-                  <li className="header__nav-item user">
-                    <a className="header__nav-link header__nav-link--profile" href="#">
-                      <div className="header__avatar-wrapper user__avatar-wrapper">
-                      </div>
-                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                      <span className="header__favorite-count">0</span>
-                    </a>
-                  </li>
-                  <li className="header__nav-item">
-                    <a className="header__nav-link" href="#">
-                      <span className="header__signout">Sign out</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-        </header>
-
-        <main className="page__main page__main--favorites page__main--favorites-empty">
-          <div className="page__favorites-container container">
-            <section className="favorites favorites--empty">
-              <h1 className="visually-hidden">Favorites (empty)</h1>
-              <div className="favorites__status-wrapper">
-                <b className="favorites__status">Nothing yet saved.</b>
-                <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
-              </div>
-            </section>
-          </div>
-        </main>
-        <footer className="footer">
-          <Link to='/' className="footer__logo-link">
-            <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
-          </Link>
-        </footer>
-      </div>
+      <FavoritesEmpty />
   );
 }
 
